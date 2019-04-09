fcd7b66af31a56c4648a7f723b964bc7b2b544bd
thomaspayton <31682200+thomaspayton@users.noreply.github.com>
2019-04-08 20:10:55 -0500

    Create dog.cs

---
+++ dog.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Pet
+{
+    public class Dog : Pet
+{
+    public Dog(string dog, string owner, int weight, int bark)
+    {
+        int number = bark;
+
+        Dog bark(number)
+
+        { while (number > 0)
+            {
+                Console.WriteLine("bark"); 
+                number--; 
+            }
+        }
+
+    }
+}
