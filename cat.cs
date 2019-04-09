db309a48a00ee754675df14be2800194d74304f2
thomaspayton <31682200+thomaspayton@users.noreply.github.com>
2019-04-08 20:11:17 -0500

    Create cat.cs

---
+++ cat.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Pet
+
+{
+    public class Cat
+    {
+        public Cat()
+        {
+            while (number > 0)
+            {
+                Console.WriteLine("meow."); 
+                number--;
+            }
+        }
+    }
+}
